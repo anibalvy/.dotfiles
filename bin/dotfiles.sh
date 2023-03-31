f38a2d49783b4412e0928c14efa821d5e83ddaf7
kanibalv <kanibalv@gmail.com>
2023-03-31 02:20:07 -0300

    new cheat, added new installer to bin, zshrc improv

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -10,7 +10,7 @@ echo "checking tools:"
 
 #OH MY ZSH 
 if [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
-	echo "   - OH MY ZSH found"
+	echo "   - OH MY ZSH ................found"
 else
 	while true; do
             read -p "OH MY ZSH not installed: do you want to install it: [Y/n}" yn
@@ -29,7 +29,7 @@ fi
 
 #OH MY ZSH - POWERLEVEL10K
 if [ -f ~/.oh-my-zsh/custom/themes/powerlevel10k/powerlevel10k.zsh-theme ]; then
-	echo "   - Powerlevel10K found"
+	echo "   - Powerlevel10K.............found"
 else
 	while true; do
             read -p "Powerlevel10K not installed: do you want to install it: [Y/n}" yn
@@ -48,13 +48,13 @@ else
 fi
 
 #Node Version Manager
-if [ -f ~/.nvm/nvm.sh ]; then
-	echo "   - nvm command found"
-else
+# if [ -f ~/.nvm/nvm.sh ]; then
+if [ $(which nvm | grep 'not found' | wc -l) = 1 ]; then 
+  echo 'not installed'; 
 	while true; do
-            read -p "nvm not installed: do you want to install it: [Y/n}" yn
-            case $yn in
-                [Yy]* ) 
+      read -p "nvm not installed: do you want to install it: [Y/n}" yn
+      case $yn in
+          [Yy]* ) 
 			curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 			break;;
                 [Nn]* ) 
@@ -63,40 +63,42 @@ else
                 * ) 
 			echo "Please answer yes or no.";;
         esac
-done
+  done
+else 
+	echo "   - nvm.......................found"
 fi
 
 #PyENV
 if [ -f ~/.pyenv/bin/pyenv ]; then
-	echo "   - pyenv command found"
+	echo "   - pyenv.....................found"
 else
 	while true; do
-            read -p "pyenv not installed: do you want to install it: [Y/n}" yn
-            case $yn in
-                [Yy]* ) 
-			git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-			echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-			echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-			echo "Luego seleccionar versiones de python con"
-			echo " pyenv global system"
-			echo " pyenv local 2.17.6"
-			echo " pyenv local 3.8.0"
-			echo "\nPara Virtual Enviroment usar pipenv"
-			echo "Instalar pipenv con pip/pip3 install pipenv"
-			echo "pipenv install"
-			break;;
-                [Nn]* ) 
-			echo "skipping pyenv installation"; 
-			break;;
-                * ) 
-			echo "Please answer yes or no.";;
+        read -p "pyenv not installed: do you want to install it: [Y/n}" yn
+        case $yn in
+            [Yy]* ) 
+                    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+                    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+                    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+                    echo "Luego seleccionar versiones de python con"
+                    echo " pyenv global system"
+                    echo " pyenv local 2.17.6"
+                    echo " pyenv local 3.8.0"
+                    echo "\nPara Virtual Enviroment usar pipenv"
+                    echo "Instalar pipenv con pip/pip3 install pipenv"
+                    echo "pipenv install"
+                    break;;
+            [Nn]* ) 
+                    echo "skipping pyenv installation"; 
+                    break;;
+            * ) 
+                    echo "Please answer yes or no.";;
         esac
-done
+  done
 fi
 
 #Set up Vundle
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
-	echo "   - Vim Vundle found"
+	echo "   - Vim Vundle................found"
 else
 	while true; do
             read -p "Vim Vundle not installed: do you want to install it: [Y/n}" yn
@@ -113,6 +115,26 @@ else
         esac
 done
 fi
+
+# RUST - RUSTUO (version manager)
+if [ $(which rustup | grep 'not found' | wc -l) = 1 ]; then 
+	while true; do
+    read -q "Install Rust Version Manager - RUSTUP[Y/n]" yn
+    case $yn in
+        [Yy]* ) 
+                curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+                break;;
+        [Nn]* ) 
+                echo "skipping pyenv installation"; 
+                break;;
+        * ) 
+                echo "Please answer yes or no.";;
+    esac
+  done
+else 
+	echo "   - rustup....................found"
+fi
+
 # Using stow to manage installation of config files
 # if a file named ".nostow" is present, that folder 
 # will not be installed
