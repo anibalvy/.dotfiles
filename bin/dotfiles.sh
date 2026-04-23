e8a36a8d72b99c7a8fcf87d9563da8155ca58716
kanibalv <kanibalv@gmail.com>
2026-04-23 01:27:27 -0400

    add pks k8s

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -11,8 +11,8 @@ export LC_ALL=en_US.UTF-8
 trap 'echo -e "\n\nCancelled by user, exiting..."; exit 130' INT
 
 # Package lists
-PKG_COMMON="git curl stow wget tmux neovim jq yq htop fastfetch rustup"
-PKG_MAC="lens bat awscli"
+PKG_COMMON="git curl stow wget tmux neovim jq yq htop fastfetch rustup lsd"
+PKG_MAC="lens bat awscli kubectl kubecolor"
 PKG_DEBIAN="batcat python3 python3-pip build-essential apt-transport-https ca-certificates gnupg"
 PKG_NEON=""
 
